643c9bcd5a4b8485f8769dc91a4e196c6a172387
witty-collection-3981 <yisroel96@gmail.com>
2018-11-09 03:10:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,7 +17,7 @@ function init() {
   document.body.addEventListener("keydown", (e) => {
     const key = e.key
     
-    idx = (codes[idx] === key) ? ++idx : 0
+    //idx = (codes[idx] === key) ? ++idx : 0
     
     if (idx === codes.length) {
       window.alert("Hurray!");
